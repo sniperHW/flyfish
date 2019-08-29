14bc8139e3c5963e3578304369e385403f4b9ef5
huangwei <sniperhw@163.com>
2019-08-29 14:50:16 +0800

    none

--- server/binlog.go
+++ server/binlog.go
@@ -13,6 +13,7 @@ import (
 )
 
 const (
+	binlog_none     = 0
 	binlog_snapshot = 1
 	binlog_update   = 2
 	binlog_delete   = 3
@@ -170,11 +171,26 @@ func (this *kvstore) flushBinlog(binlogStr *str, ctxs *ctxArray, cacheBinlogCoun
 	this.mtx.Unlock()
 
 	if nil != ctxs {
+		/*   binlog持久化完成
+		 *   响应客户端并把更新提交到内存
+		 */
 		for i := 0; i < ctxs.count; i++ {
 			v := ctxs.ctxs[i]
 			v.reply(errcode.ERR_OK, v.fields, v.version)
 			ckey := v.getCacheKey()
 			ckey.mtx.Lock()
+
+			ckey.snapshoted = true
+
+			if v.writeBackFlag == write_back_insert || v.writeBackFlag == write_back_update || v.writeBackFlag == write_back_insert_update {
+				ckey.setValueNoLock(v)
+				ckey.setOKNoLock(v.version)
+			} else {
+				ckey.setMissingNoLock()
+			}
+
+			ckey.sqlFlag = v.writeBackFlag
+
 			if !ckey.writeBackLocked {
 				ckey.writeBackLocked = true
 				pushSqlWriteReq(ckey)
@@ -258,6 +274,7 @@ func (this *kvstore) writeBinlog(tt int, unikey string, fields map[string]*proto
 	}
 }
 
+/*
 func (this *kvstore) checkCacheKey(ckey *kv, ctx *cmdContext) bool {
 	ckey.mtx.Lock()
 
@@ -341,6 +358,123 @@ func (this *kvstore) checkCacheKey(ckey *kv, ctx *cmdContext) bool {
 		return true
 	}
 }
+*/
+
+func fillDefaultValue(meta *table_meta, ctx *cmdContext) {
+	for _, v := range meta.fieldMetas {
+		defaultV := proto.PackField(v.name, v.defaultV)
+		if _, ok := ctx.fields[v.name]; !ok {
+			ctx.fields[v.name] = defaultV
+		}
+	}
+}
+
+func (this *kvstore) checkCacheKey(ckey *kv, ctx *cmdContext) (bool, int) {
+	ckey.mtx.Lock()
+
+	gotErr := false
+	binop := binlog_none
+	sqlFlag := write_back_none
+
+	switch ckey.sqlFlag {
+	case write_back_none:
+		sqlFlag = ctx.writeBackFlag
+	case write_back_insert, write_back_insert_update:
+		if ctx.writeBackFlag == write_back_update {
+			sqlFlag = write_back_insert_update
+		} else if ctx.writeBackFlag == write_back_delete {
+			sqlFlag = write_back_delete
+		} else {
+			gotErr = true
+			Errorln("invaild ctx.writeBackFlag")
+		}
+	case write_back_delete:
+		if ctx.writeBackFlag == write_back_insert {
+			sqlFlag = write_back_insert
+		} else {
+			gotErr = true
+			Errorln("invaild ctx.writeBackFlag")
+		}
+	case write_back_update:
+		if ctx.writeBackFlag == write_back_update {
+			sqlFlag = write_back_update
+		} else if ctx.writeBackFlag == write_back_delete {
+			sqlFlag = write_back_delete
+		} else {
+			gotErr = true
+			Errorln("invaild ctx.writeBackFlag")
+		}
+	default:
+		gotErr = true
+		Errorln("invaild ctx.writeBackFlag")
+	}
+
+	if gotErr {
+		ckey.mtx.Unlock()
+		ctx.reply(errcode.ERR_ERROR, nil, -1)
+		ckey.processQueueCmd()
+		return false, binop
+	} else {
+
+		cmdType := ctx.getCmdType()
+
+		if cmdType != cmdDel && nil == ckey.values {
+			fillDefaultValue(ckey.getMeta(), ctx)
+		}
+
+		switch cmdType {
+		case cmdIncrBy, cmdDecrBy:
+			cmd := ctx.getCmd()
+			var newV *proto.Field
+			var oldV *proto.Field
+			if nil != ckey.values {
+				oldV = ckey.values[cmd.incrDecr.GetName()]
+			} else {
+				oldV = ctx.fields[cmd.incrDecr.GetName()]
+			}
+			if cmdType == cmdIncrBy {
+				newV = proto.PackField(cmd.incrDecr.GetName(), oldV.GetInt()+cmd.incrDecr.GetInt())
+			} else {
+				newV = proto.PackField(cmd.incrDecr.GetName(), oldV.GetInt()-cmd.incrDecr.GetInt())
+			}
+			ctx.fields[newV.GetName()] = newV
+			ctx.version = ckey.version + 1
+		case cmdDel:
+			ctx.version = 0
+		case cmdSet, cmdSetNx, cmdCompareAndSet, cmdCompareAndSetNx:
+			ctx.version = ckey.version + 1
+		}
+
+		switch sqlFlag {
+		case write_back_delete:
+			if ckey.snapshoted {
+				binop = binlog_delete
+			} else {
+				binop = binlog_snapshot
+			}
+		case write_back_insert, write_back_insert_update:
+			binop = binlog_snapshot
+		case write_back_update:
+			if ckey.snapshoted {
+				binop = binlog_update
+			} else {
+				binop = binlog_snapshot
+			}
+		}
+
+		if binop == binlog_snapshot && sqlFlag != write_back_delete && ckey.values != nil {
+			for k, v := range ckey.values {
+				if _, ok := ctx.fields[k]; !ok {
+					ctx.fields[k] = v
+				}
+			}
+		}
+
+		ckey.mtx.Unlock()
+		ctx.writeBackFlag = sqlFlag
+		return true, binop
+	}
+}
 
 func (this *kvstore) processUpdate(ctx *cmdContext) {
 
@@ -350,12 +484,18 @@ func (this *kvstore) processUpdate(ctx *cmdContext) {
 
 	ckey := ctx.getCacheKey()
 
-	if !this.checkCacheKey(ckey, ctx) {
+	//if !this.checkCacheKey(ckey, ctx) {
+	//	return
+	//}
+
+	ok, binop := this.checkCacheKey(ckey, ctx)
+
+	if !ok {
 		return
 	}
 
 	this.mtx.Lock()
-	ckey.mtx.Lock()
+	//ckey.mtx.Lock()
 
 	if nil == this.ctxs {
 		this.ctxs = ctxArrayGet()
@@ -363,7 +503,7 @@ func (this *kvstore) processUpdate(ctx *cmdContext) {
 
 	this.ctxs.append(ctx)
 
-	switch ckey.sqlFlag {
+	/*switch ckey.sqlFlag {
 	case write_back_delete:
 		if ckey.snapshoted {
 			this.writeBinlog(binlog_delete, ckey.uniKey, nil, 0)
@@ -381,9 +521,15 @@ func (this *kvstore) processUpdate(ctx *cmdContext) {
 			ckey.snapshoted = true
 			this.writeBinlog(binlog_snapshot, ckey.uniKey, ckey.values, ckey.version)
 		}
+	}*/
+
+	//ckey.mtx.Unlock()
+
+	if len(ctx.fields) == 0 || ctx.version == 0 {
+		panic("len(ctx.fields == 0) || ctx.version == 0")
 	}
 
-	ckey.mtx.Unlock()
+	this.writeBinlog(binop, ckey.uniKey, ctx.fields, ctx.version)
 
 	this.tryFlushBinlog()
 
