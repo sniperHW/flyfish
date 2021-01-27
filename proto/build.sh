1258fe70074e386b6b888aacf3ac91ba59ecff3d
huangwei <sniperhw@163.com>
2021-01-27 16:13:30 +0800

    none

--- proto/build.sh
+++ proto/build.sh
@@ -1,4 +1,4 @@
 #all:
 #	protoc --gofast_out=. *.proto
 #protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/proto --gogofaster_out=. *.proto
-protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/proto --gogoslick_out=. *.proto
+protoc -I=. --gogoslick_out=../../flyfish/proto *.proto
