c3067602ff807ec898b3ee50de451cc2ba01f9c0
huangwei <sniperhw@163.com>
2021-09-22 14:37:54 +0800

    hash_dev

--- server/pd/proto/build.sh
+++
@@ -1,4 +0,0 @@
-#all:
-#	protoc --gofast_out=. *.proto
-#protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/proto --gogofaster_out=. *.proto
-protoc -I=. --gofast_out=../../pd/proto *.proto
